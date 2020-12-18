459b5d3f0795e99a5060390df5fa81f106616cb2
Deepti Teragunti <67197586+DeeptiTeragunti@users.noreply.github.com>
2020-12-18 13:05:12 +0530

    Add files via upload
    success pages updated

---
+++ Accountsuccess.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './accountsuccess.css'
+import DoneIcon from '@material-ui/icons/Done';
+function Accountsuccess() {
+    return (
+        <div className="container">
+            <div className="tons">
+
+             <p><DoneIcon></DoneIcon>Email verified</p>
+             <p><DoneIcon>
+                 </DoneIcon>Phone verified
+             </p>
+            </div>
+        <div className="para">
+        <p >
+            Account Creation 
+        </p>
+        </div>
+        <p>
+            Successful
+        </p>
+    </div>
+    )
+}
+
+export default Accountsuccess
