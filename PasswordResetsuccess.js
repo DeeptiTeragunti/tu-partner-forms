507a30c470734ebb6580b6ceb3f1a55174674ea9
Deepti Teragunti <67197586+DeeptiTeragunti@users.noreply.github.com>
2020-12-18 12:48:45 +0530

    Add files via upload

---
+++ PasswordResetsuccess.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './passwordsuccess.css'
+
+function PasswordResetsuccess() {
+    return (
+        <div className="container">
+            <div className="para">
+            <p >
+                Password Reset
+            </p>
+            </div>
+            <p>
+                Successful
+            </p>
+        </div>
+    )
+}
+
+export default PasswordResetsuccess
